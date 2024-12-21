5b40bae4390d2b5443ac201e9d0e7f07e9040c54
Jonathan <machadofjonathan@gmail.com>
2024-12-21 22:12:25 +0000

    chore: uv setup and pytest addition

---
+++ server/test/test_api.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+import pytest
+from src.main import app
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+def test_hello_world(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
