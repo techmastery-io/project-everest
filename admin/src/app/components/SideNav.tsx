b9e442a982aba509bd0ee8bce0dad8b6136521f5
Jonathan <machadofjonathan@gmail.com>
2024-12-23 20:19:32 +0000

    chore: refactor SideNav component and add Content page

--- admin/src/app/components/SideNav.tsx
+++ admin/src/app/components/SideNav.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { ReactNode } from "react";
 
 export default function SideNav() {
   return (
@@ -6,20 +7,28 @@ export default function SideNav() {
       <div className="flex w-72 flex-col bg-gray-200">
         <nav>
           <ul>
-            <li>
-              <a href="">Content</a>
-            </li>
-            <li>
-              <Link
-                href="/content-type"
-                className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-              >
-                Content Type
-              </Link>
-            </li>
+            <NavItem href="/content">Content</NavItem>
+            <NavItem href="/content-type">Content Types</NavItem>
           </ul>
         </nav>
       </div>
     </div>
   );
+}
+
+
+const NavItem = ({ href, children }: NavItemProps) => (
+  <li>
+    <Link
+      href={href}
+      className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+    >
+      {children}
+    </Link>
+  </li>
+);
+
+interface NavItemProps {
+  href: string;
+  children: ReactNode;
 }
